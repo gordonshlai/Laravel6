b3f04d0f10d4b9f86056b9411a82d17d161cfc99
Gordon Lai <gordonshlai@yahoo.com.hk>
2021-01-28 13:06:28 +0000

    Added database access.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,23 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use DB;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function show($post)
+    public function show($slug)
     {
-        $posts = [
-            'my-first-post' => 'Hello, this is my first blog post!',
-            'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-        ];
-
-        if (!array_key_exists($post, $posts)) {
-            abort(404, 'Sorry, that post was not found.');
-        }
-
         return view('posts', [
-            'post' => $posts[$post]
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
